bfb9f397d62f239144a854ff131554ddf248d4a5
Shoxjaxon-com <alijonovshohjahon03@gmail.com>
2024-10-04 05:57:07 -0700

    vazifa

---
+++ index.js
@@ -0,0 +1,186 @@
+// === Math Functions === //
+// 1-masala
+
+// let num = Math.trunc(Math.random() * 100)
+// console.log(num);
+
+// 2-masala
+
+// let num = +prompt('Sonni kiriting')
+
+// if(num >= 0){
+//     let ildiz = Math.sqrt(num)
+// }else{
+//     console.log('Musbat son kiriting');
+// }
+
+
+// 3-masala
+
+//     let num1 =+prompt('1-sonni kiriting')
+//     let num2 =+prompt('2-sonni kiriting')
+//     let num3 =+prompt('3-sonni kiriting')
+//     let num4 =+prompt('4-sonni kiriting')
+//     let num5 =+prompt('5-sonni kiriting')
+
+//    let max = Math.max(num1,num2,num3,num4,num5)
+//    let min = Math.min(num1,num2,num3,num4,num5)
+
+//    console.log("Katta son" + max);
+//    console.log('Kichik son' + min);
+
+// == Ternary operator == //
+
+// 1-masala
+// let num = +prompt('Sonni kiriting')
+// console.log((num>0)  ? 'Musbat son' : 'Manfiy son');
+
+// 2-masala
+
+// let age = +prompt('Sonni kiritng')
+// console.log((age >= 18) ? 'Ruxsat etilgan' : 'Ruxsat etilamgan');
+
+// 3-masala
+// let num = +prompt('Sonni kiriting')
+// console.log((num % 2 == 1) (num % 2 == 0) ? 'Juft son' : 'Toq son');
+
+
+
+
+
+
+
+
+//  ==== FOR  ==== //
+
+// 1-masala
+
+// for(let i = 1; i <= 20; i++){
+//     if(i % 2 == 0){
+//         console.log(i);
+//     }
+// }
+
+// 2-masala
+
+// let sum = 0;
+// for(let i = 1;  i <= 10 ; i++ ){
+//     let num =+prompt("Sonni kiriting")
+//     if(i > 0){
+//         sum = sum + i
+//     }
+// }
+// console.log(sum);
+
+// 3-masala
+// for(let i = 1; i<= 100; i++){
+//     if(i > 30 && i > 50){
+//         console.log(i);
+//     }
+// }
+
+// 4-masala
+
+// let musbat = 0;
+// let manfiy = 0;
+// for( let i = 1; i<= 15; i++){
+//     let num =+prompt('Sonni kiriting')
+//     if(num >= 0){
+//         musbat++
+//     }
+//     if(num <= 0){
+//         manfiy++
+//     }
+// }
+// console.log("Musbat son"+' ' + musbat);
+// console.log('Manfiy son' + ' ' + manfiy);
+
+// 6-masala
+
+// let sum = 0 ;
+// for(let i = 1; i <= 50 ; i++){
+//     if(i % 2 == 1){
+//         sum+=i
+//     }
+// }
+// console.log(sum);
+
+// 7-masala
+
+// for(let i = 1; i <= 5 ; i++){
+//     let age =+prompt('Yoshigizni kiriting')
+//     if(i <= 18){
+//         console.log('Voyaga yetgan 👤');
+//     }else{
+//         console.log('Voyaga yetmagan 👶 ');
+//     }
+// }
+
+// 8-masala
+// let juftSon = 0;
+// for(let i = 1; i <= 100; i++){
+//     if( i % 2 == 0){
+//         juftSon++;
+//     }
+// }
+// console.log(juftSon);
+
+// 9-masala
+// let counter = 0;
+// for(let i = 1 ; i<= 10 ; i++ ){
+//     let num =+prompt('Sonni kiriting')
+//     if( num > 5 && num > 10){
+//         counter++
+//     }
+// }
+// console.log(counter);
+
+// 10-masala
+// for(let i = 1; i < 10 ; i++){
+//     let num =+prompt('Sonni kiriting')
+//     if(num > 0){
+//         console.log('Musbat sonlar' + ' ' + num);
+//     }
+// }
+
+// 11-masala
+
+// let juft = 0;
+// let manfiy = 0;
+// for(let i = 1; i<= 15 ; i++){
+//     let num = +prompt('Sonni kirinting')
+//     if(num % 2 == 0 || num < 0 ){
+//         juft++;
+//         manfiy++
+//     }
+//     console.log('Juft sonllar' + juft);
+//     console.log('Manfiy sonllar' + manfiy);
+// }
+
+// 12-masala
+
+// for(let i = 1; i <= 50; i++){
+//     if(i % 3 == 0){
+//         console.log(i);
+//     }
+// }
+
+// 13-masala
+
+// for(let i = 1; i<=10; i++){
+//     let num =+prompt('Sonni kiriting')
+//     if(num > 50){
+//         console.log(num);
+//     }
+// }
+
+// 14-masala
+
+// for(let i = 1; i <= 10; i++){
+//     let num = +prompt('Sonni kiriting')
+
+//     if(num === 0){
+//         console.log('Mavjud');
+//     }
+
+// }
